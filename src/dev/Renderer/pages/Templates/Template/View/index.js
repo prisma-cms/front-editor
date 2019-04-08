d0fb0d4019f78a1236008c987e720d9a32726ab4
Fi1osof <n.lanets@modxclub.ru>
2019-04-08 03:12:45 +0300

    Cleanup

--- src/dev/Renderer/pages/Templates/Template/View/index.js
+++ src/dev/Renderer/pages/Templates/Template/View/index.js
@@ -1,128 +1,128 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import EditableView from "apollo-cms/lib/DataView/Object/Editable";
+// import EditableView from "apollo-cms/lib/DataView/Object/Editable";
 
-import { withStyles, IconButton } from 'material-ui';
+// import { withStyles, IconButton } from 'material-ui';
 
 import FrontEditor from "../../../../../../App";
 
-export const styles = theme => {
+// export const styles = theme => {
 
-  return {
-    root: {
-      // border: "1px solid blue",
-      height: "100%",
-      display: "flex",
-      flexDirection: "column",
-    },
-    // membersShortList: {
-    //   alignItems: "start",
-    //   display: "inline-flex",
+//   return {
+//     root: {
+//       // border: "1px solid blue",
+//       height: "100%",
+//       display: "flex",
+//       flexDirection: "column",
+//     },
+//     // membersShortList: {
+//     //   alignItems: "start",
+//     //   display: "inline-flex",
 
-    //   "& > *": {
-    //     margin: 2,
-    //   },
-    // },
-    editor: {
-      flex: 1,
-      display: "flex",
-      flexDirection: "column",
-    },
-    // messages: {
-    //   overflow: "hidden",
-    //   // border: "1px solid yellow",
-    //   flex: 1,
-    // },
-    // editor: {
-    //   // border: "1px solid red",
-    // },
-  }
-}
-
-
-
-export class TemplateView__ extends EditableView {
+//     //   "& > *": {
+//     //     margin: 2,
+//     //   },
+//     // },
+//     editor: {
+//       flex: 1,
+//       display: "flex",
+//       flexDirection: "column",
+//     },
+//     // messages: {
+//     //   overflow: "hidden",
+//     //   // border: "1px solid yellow",
+//     //   flex: 1,
+//     // },
+//     // editor: {
+//     //   // border: "1px solid red",
+//     // },
+//   }
+// }
 
 
-  constructor(props) {
 
-    super(props);
-
-    this.updateObject = this.updateObject.bind(this);
-    // this.updateComponentProperty = this.updateComponentProperty.bind(this);
-    // this.onChangeProps = this.onChangeProps.bind(this);
-    // this.updateProps = this.updateProps.bind(this);
-
-  }
+// export class TemplateView__ extends EditableView {
 
 
-  canEdit() {
+//   constructor(props) {
 
-    const {
-      id,
-      CreatedBy,
-    } = this.getObjectWithMutations() || {}
+//     super(props);
 
+//     this.updateObject = this.updateObject.bind(this);
+//     // this.updateComponentProperty = this.updateComponentProperty.bind(this);
+//     // this.onChangeProps = this.onChangeProps.bind(this);
+//     // this.updateProps = this.updateProps.bind(this);
 
-    const {
-      id: createdById,
-    } = CreatedBy || {};
-
-    const {
-      id: currentUserId,
-    } = this.getCurrentUser() || {};
-
-    return !id || (currentUserId && currentUserId === createdById) ? true : false;
-  }
+//   }
 
 
-  renderDefaultView() {
+//   canEdit() {
 
-    const {
-      Grid,
-    } = this.context;
+//     const {
+//       id,
+//       CreatedBy,
+//     } = this.getObjectWithMutations() || {}
 
-    const {
-      classes,
-      ...other
-    } = this.props;
 
-    // const object = this.getObjectWithMutations();
-    // const canEdit = this.canEdit();
-    // const inEditMode = this.isInEditMode();
+//     const {
+//       id: createdById,
+//     } = CreatedBy || {};
 
-    return <div
-      className={classes.editor}
-    >
-      <FrontEditor
-        inEditMode={true}
-        // object={object}
-        {...other}
-      />
-    </div>;
+//     const {
+//       id: currentUserId,
+//     } = this.getCurrentUser() || {};
 
-  }
+//     return !id || (currentUserId && currentUserId === createdById) ? true : false;
+//   }
 
-  renderEditableView() {
 
-    return this.renderDefaultView();
-  }
+//   renderDefaultView() {
 
-  render() {
+//     const {
+//       Grid,
+//     } = this.context;
 
-    const {
-      classes,
-    } = this.props;
+//     const {
+//       classes,
+//       ...other
+//     } = this.props;
 
-    return <div
-      className={classes.root}
-    >
-      {super.render()}
-    </div>
-  }
+//     // const object = this.getObjectWithMutations();
+//     // const canEdit = this.canEdit();
+//     // const inEditMode = this.isInEditMode();
 
-}
+//     return <div
+//       className={classes.editor}
+//     >
+//       <FrontEditor
+//         inEditMode={true}
+//         // object={object}
+//         {...other}
+//       />
+//     </div>;
+
+//   }
+
+//   renderEditableView() {
+
+//     return this.renderDefaultView();
+//   }
+
+//   render() {
+
+//     const {
+//       classes,
+//     } = this.props;
+
+//     return <div
+//       className={classes.root}
+//     >
+//       {super.render()}
+//     </div>
+//   }
+
+// }
 
 
 export class TemplateView extends Component {
@@ -142,6 +142,8 @@ export class TemplateView extends Component {
   }
 }
 
-export default withStyles(styles)(props => <TemplateView
-  {...props}
-/>);
+export default TemplateView;
+
+// export default withStyles(styles)(props => <TemplateView
+//   {...props}
+// />);
