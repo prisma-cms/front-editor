03a89ee9e829f02d66a181d2d4f65e1751f5856a
Fi1osof <n.lanets@modxclub.ru>
2020-11-21 14:24:22 +0300

    Added appRender test util

--- dev/tests/utils.tsx
+++ dev/tests/utils.tsx
@@ -1,9 +1,7 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useMemo } from 'react'
 import ReactDOMServer from 'react-dom/server'
-// import {createRouter} from 'next/router'
 import { render as baseRender, RenderResult } from '@testing-library/react'
-
-// import App from 'pages/_app'
+import App from '../pages/_App'
 import theme from '../theme'
 import { ThemeProvider } from 'styled-components'
 
@@ -46,6 +44,25 @@ const HeadProvider: React.FC = ({ children }) => {
       {children}
     </HeadManagerContext.Provider>
   )
+}
+
+/**
+ * Renderer with main App
+ */
+const AppProvider: React.FC = ({ children }: any, pageProps: any) => {
+  const Component = useMemo(() => {
+    return () => children
+  }, [children])
+
+  return (
+    <HeadProvider>
+      {/* @ts-ignore */}
+      <App Component={Component} pageProps={pageProps} />
+    </HeadProvider>
+  )
+}
+export const appRender = (ui: ReactElement) => {
+  return baseRender(ui, { wrapper: AppProvider }) as RenderResult
 }
 
 /**
