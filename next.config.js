9e7b5d9332d4c053d99174c3625f0b3fadcc420b
Fi1osof <n.lanets@modxclub.ru>
2020-11-22 03:22:25 +0300

    Fix storybook

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+
 const webpack = (config) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
@@ -12,11 +14,11 @@ const webpack = (config) => {
    * Fix locales issue
    * https://github.com/moment/moment/issues/2517#issuecomment-620674018
    */
-  // config.plugins.push(
-  //   new MomentLocalesPlugin({
-  //     localesToKeep: ['ru'],
-  //   })
-  // )
+  config.plugins.push(
+    new MomentLocalesPlugin({
+      localesToKeep: ['ru', 'en'],
+    })
+  )
 
   // Object.assign(config, {
   //   // https://nextjs.org/docs/api-reference/next.config.js/disabling-etag-generation
