923a850007e0bf0eeac63980a89a04f7a8a47ad4
Fi1osof <n.lanets@modxclub.ru>
2020-11-23 18:21:08 +0300

    Move MainPage into dev

---
+++ dev/pages/MainPage/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+import App from '../../../src'
+
+const MainPage: React.FC = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Component boilerplate</title>
+        <meta
+          name="description"
+          content="Component boilerplate for prisma-cms"
+        />
+      </Head>
+      <App {...props} />
+    </>
+  )
+}
+
+export default MainPage
